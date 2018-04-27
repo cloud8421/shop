e075b60f5d8b33043e544d4c972ce782c960796b
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-04-27 14:18:22 +0100

    Extends product router tests

--- apps/front/test/front/router/product_test.exs
+++ apps/front/test/front/router/product_test.exs
@@ -16,4 +16,26 @@ defmodule Front.Router.ProductTest do
     assert conn.status == 200
     assert conn.resp_body == "[]"
   end
+
+  test "GET / with products" do
+    Store.clear()
+
+    product_one =
+      Store.Product.new(%{sku: "p123", name: "Printer-one", description: "Some printer"})
+
+    product_two =
+      Store.Product.new(%{sku: "p456", name: "Printer-two", description: "Some printer"})
+
+    :ok = Store.insert(product_one)
+    :ok = Store.insert(product_two)
+
+    conn =
+      conn(:get, "/")
+      |> R.call(@opts)
+
+    assert conn.status == 200
+
+    assert conn.resp_body ==
+             ~s([{"description":"Some printer","name":"Printer-two","sku":"p456"},{"description":"Some printer","name":"Printer-one","sku":"p123"}])
+  end
 end
