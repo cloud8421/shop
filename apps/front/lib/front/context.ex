3ccbc15e9ad2aeb963b884398d19b06fc00dfee3
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-05-07 09:08:25 +0100

    Extract functions to context struct module

--- apps/front/lib/front/context.ex
+++ apps/front/lib/front/context.ex
@@ -1,3 +1,6 @@
 defmodule Front.Context do
   defstruct store: Store
+
+  def new, do: %__MODULE__{}
+  def new(attrs), do: struct(__MODULE__, attrs)
 end
--- apps/front/lib/front/plug/context.ex
+++ apps/front/lib/front/plug/context.ex
@@ -4,16 +4,10 @@ defmodule Front.Plug.Context do
   def call(conn, _opts), do: with_defaults(conn)
 
   def with_defaults(conn) do
-    Plug.Conn.put_private(conn, :context, %Front.Context{})
+    Plug.Conn.put_private(conn, :context, Front.Context.new())
   end
 
   def with_overrides(conn, overrides) do
-    context = struct(Front.Context, overrides)
-    Plug.Conn.put_private(conn, :context, context)
-  end
-
-  def assign(conn, key, value) do
-    new_context = %{conn.private.context | key => value}
-    Plug.Conn.put_private(conn, :context, new_context)
+    Plug.Conn.put_private(conn, :context, Front.Context.new(overrides))
   end
 end
